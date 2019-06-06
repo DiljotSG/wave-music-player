f2a913ed86127b0d377e674c52a14a961447ca9d
Diljot Garcha <garchads@myumanitoba.ca>
2019-06-05 19:48:06 -0500

    [#87] Automatically scan the default directory for songs
    Additional issues solved:
    - [#40, #41, #43], Display the name, artist, and album for a song.

---
+++ WaveMusic/app/src/test/java/com/team_ten/wavemusic/tests/TestSongClass.java
@@ -0,0 +1,37 @@
+package com.team_ten.wavemusic.tests;
+
+// Import statements
+import com.team_ten.wavemusic.Song;
+import org.junit.Test;
+
+public class TestSongClass
+{
+    @Test
+    public void TestEqualsMethod()
+    {
+        // Create 3 Song Objects. songOne and songThree have the same URI, while that of songTwo is different.
+        Song songOne = new Song(
+                "title1",
+                "artist1",
+                "album1",
+                "uri1"
+        );
+        Song songTwo = new Song(
+                "title2",
+                "artist2",
+                "album2",
+                "uri2"
+        );
+        Song songThree = new Song(
+                "title1",
+                "artist1",
+                "album1",
+                "uri1"
+        );
+
+        // Since two Song Objects are equal if they have the same URI,
+        // songOne and songTwo should NOT be equal and songOne and songThree should be equal.
+        assert (!songOne.equals(songTwo));
+        assert (songOne.equals(songThree));
+    }
+}
